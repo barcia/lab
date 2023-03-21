d316dbc2233fe4a3701921d52fff913c1b7f527d
Iván Barcia <ivanbarcia@icloud.com>
2023-03-21 23:12:38 +0100

    Simplify and convert it to custom element

--- dialog/script.js
+++ dialog/script.js
@@ -1,32 +1,49 @@
 const isEscKey = ev => ev.key === 'Escape' || ev.keyCode === 27;
 
 
-class Modal {
-	constructor(element) {
-		this.element = element
-		this.closer = element.querySelector('#close')
+class Dialog extends HTMLElement {
+	constructor() {
+		super()
+		this.element = this.querySelector('dialog')
+		this.openers = Array.from(document.querySelectorAll(`[aria-controls="${this.element.id}"]`))
+		this.closers = Array.from(this.querySelectorAll('.js-close'))
 		this.body = document.body
 		this.lockScroll = true;
+
+		this.#init()
+	}
+
+	#init() {
+		this.#activeOpeners()
+		this.#activeClosers()
+		this.#closeOnClickBackdrop()
+		this.#closeOnClickEscKey()
+	}
+
+	#closeOnClickBackdrop() {
+		this.element.addEventListener('click', ev => ev.target.nodeName === 'DIALOG' && this.close())
+	}
+
+	#closeOnClickEscKey() {
+		document.addEventListener('keydown', ev => {
+			if (this.element.open && isEscKey(ev)) {
+				ev.preventDefault()
+				this.close()
+			}
+		})
+	}
+
+	#activeOpeners() {
+		if (this.openers.length > 0) this.openers.forEach(opener => opener.addEventListener('click', () => this.show()))
+	}
+
+	#activeClosers() {
+		if (this.closers.length > 0) this.closers.forEach(closer => closer.addEventListener('click', () => this.close()))
 	}
 
 	show() {
 		this.element.showModal()
 		if (this.lockScroll) this.body.style.overflowY = 'hidden'
-
-		this.element.addEventListener('click', (ev) => {
-			if (ev.target.nodeName === 'DIALOG') {
-				this.close()
-			}
-		})
-
-		this.closer.addEventListener('click', () => {
-			this.close()
-		}, { once: true } )
-
-		document.addEventListener('keydown', ev => {
-			ev.preventDefault()
-			if (isEscKey(ev)) this.close()
-		}, { once: true } )
 	}
 
 	close() {
@@ -39,3 +56,5 @@ class Modal {
 		}, { once: true })
 	}
 }
+
+customElements.define("custom-dialog", Dialog);
