6749e8ff693511a8926bbc63b9275a1fc4b9416b
Iván Barcia <ivanbarcia@icloud.com>
2023-10-04 18:52:50 +0200

    Update and fix Marquee

--- marquee/script.js
+++ marquee/script.js
@@ -1,17 +1,31 @@
+const throttle = (fn, delay) => {
+    let timeout = undefined;
+
+    return (...args) => {
+        if (!timeout) {
+            timeout = setTimeout( () => {
+                fn(...args);
+                timeout = undefined;
+            }, delay)
+        }
+    }
+}
+
+
 /**
- * Marquee
- * 
- * @param {HTMLElement} element - The marquee HTML element with only one direct child
- * @param {Number} [speed=undefined] - Speed in pixeles / second
+ * Creates a Marquee instance.
+ * @class
+ * @param {HTMLElement} element - The element to apply the marquee effect.
+ * @param {number} [speed=50] - The speed of the marquee effect.
  */
 class Marquee {
-	constructor(element, speed = undefined) {
+	constructor(element, speed = 50) {
 		this.element = element
-		this.elementClone = element.cloneNode(true)
-		this.content = element.firstElementChild
-		this.allContent = [this.content]
+		this.elementClone = this.element.cloneNode(true)
+		this.slider = this.element.firstElementChild
+		this.content = Array.from(this.slider.children)
 		this.elementWidth = this.element.getBoundingClientRect().width;
-		this.contentWidth = this.content.getBoundingClientRect().width;
+		this.sliderWidth = this.slider.getBoundingClientRect().width;
 		this.speed = speed;
 
 		this.#validate() && this.#init()
@@ -26,28 +40,37 @@ class Marquee {
 		return true;
 	}
 
-	#init() {
-		for (let index = 0; index < Math.ceil(this.elementWidth / this.contentWidth); index++) {
-			const clone = this.content.cloneNode(true)
-			clone.ariaHidden = true
-			this.allContent.push(clone)
-			this.element.appendChild(clone)
-		}
-		this.#setUnitSpeed()
-		this.#eventListenerResize()
+	#setTranslateX() {
+		this.element.style.setProperty('--marquee-translateX', `-${this.sliderWidth}px`);
 	}
 
-	#setUnitSpeed() {
-		const unitSpeed = this.speed ? window.innerWidth / this.speed : undefined
-		if (unitSpeed) {
-			this.allContent.forEach(item => {
-				item.style.animationDuration = `${unitSpeed / (this.elementWidth / this.contentWidth)}s`
-			})
+	#setSpeed() {
+		const unitSpeed = window.innerWidth / this.speed
+		this.element.style.setProperty('--marquee-animation-duration', `${unitSpeed / (this.elementWidth / this.sliderWidth)}s`);
+	}
+
+	#cloneContent() {
+		for (let index = 0; index < Math.ceil(this.elementWidth / this.sliderWidth); index++) {
+			for (const item of this.content) {
+				const clone = item.cloneNode(true)
+				clone.ariaHidden = true
+				this.slider.appendChild(clone)
+			}
 		}
 	}
 
 	#eventListenerResize() {
-		window.addEventListener('resize', () => this.reset())
+		window.addEventListener('resize', throttle(() => {
+			this.reset();
+		}, 10), { once: true });
+	}
+
+	#init() {
+		this.#setTranslateX()
+		this.#setSpeed()
+		this.#cloneContent()
+		this.#eventListenerResize()
+		this.play()
 	}
 
 	reset() {
@@ -56,16 +79,11 @@ class Marquee {
 		return new Marquee(this.elementClone, this.speed)
 	}
 
-	setSpeed(speed) {
-		this.speed = speed
-		this.#setUnitSpeed()
-	}
-
 	pause() {
-		this.allContent.forEach(item => item.style.animationPlayState = 'paused')
+		this.slider.style.animationPlayState = 'paused'
 	}
 
 	play() {
-		this.allContent.forEach(item => item.style.animationPlayState = 'running')
+		this.slider.style.animationPlayState = 'running'
 	}
 }
