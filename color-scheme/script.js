4b0aa8efefca074d487afd6eec211b38b8ea0690
Iván Barcia <ivanbarcia@icloud.com>
2023-12-01 16:58:07 +0100

    Update Color Scheme handle

--- color-scheme/script.js
+++ color-scheme/script.js
@@ -1,26 +1,65 @@
-const getCheckedColorScheme = options => {
-    return options.find(opt => opt.checked)?.value;
+
+
+class ColorScheme {
+    constructor() {
+        this.default = 'auto';
+        this.selector = document.querySelector('#colorscheme-selector');
+        this.options = [...this.selector.querySelectorAll('input[name="colorscheme"]')];
+        this.current = undefined
+        this.init();
+    }
+
+    init() {
+        this.#getDefaultOption();
+        this.#setOption(this.options.find(opt => opt.value === this.current))
+        this.#handleChangeColorScheme();
+        this.#handleSystemColorSchemeChange();
+    }
+
+    #getDefaultOption() {
+        this.current = this.options.find(opt => opt.checked)?.value || this.default;
+    }
+
+    #setOption(option, value = true) {
+        option.checked = value;
+    }
+
+    #handleChangeColorScheme() {
+        this.selector.addEventListener('change', (event) => {
+            this.current = event.target.value;
+            this.#setColorScheme(event.target.value);
+        })
+    }
+
+    #handleSystemColorSchemeChange() {
+        window.matchMedia('(prefers-color-scheme: dark)')
+            .addEventListener('change',() => {
+                if (this.current !== 'auto') return;
+                this.#setColorScheme('auto')
+        })
+    }
+
+    #setColorScheme(value) {
+        value === 'auto' ? value = this.#getSystemColorScheme() : value;
+        this.#setColorSchemeOnHTML(value);
+        this.#setColorSchemeOnLocalStorage(value);
+    }
+
+    #getSystemColorScheme() {
+        return window.matchMedia ? window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light' : undefined;
+    }
+
+    #setColorSchemeOnHTML(value) {
+        document.documentElement.setAttribute('data-colorscheme', value);
+    }
+
+    #setColorSchemeOnLocalStorage(value) {
+        localStorage.setItem('colorscheme', value);
+    }
 }
 
-const getSystemColorScheme = () => {
-    return window.matchMedia ? window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light' : undefined;
-}
-
-const setColorScheme = value => {
-    let colorScheme = value === 'auto' ? value = getSystemColorScheme() : value;
-    colorScheme = value === undefined ? 'light' : value;
-    document.documentElement.setAttribute('data-colorscheme', colorScheme);
-}
+new ColorScheme();
 
 
 
-// Get all avaiable options
-const inputs = [...document.querySelectorAll('input[name="colorscheme"]')];
 
-// Get default select option, if exists
-setColorScheme(getCheckedColorScheme(inputs))
-
-// Update when the user changes the color scheme
-inputs.forEach(input => {
-    input.addEventListener('change', (event) => setColorScheme(event.target.value));
-})
